614c0901ddd8e01578f3023b50fae242e1423155
Svirin-S <svirin.sergey92@mail.ru>
2023-01-23 19:56:05 +0300

    22

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -16,12 +16,12 @@ class StockProductSerializer(serializers.ModelSerializer):
 
 
 class StockSerializer(serializers.ModelSerializer):
-    positions = StockProductSerializer(read_only=True, many=True) 
+    positions = StockProductSerializer(read_only=True, many=True)
 
     class Meta:
         model = Stock
-        fields = ['id' ,'address']    
-
+        fields = ['id', 'address', 'positions']
+    
     def create(self, validated_data):
         positions = validated_data.pop('positions', [])
         stock_product = super().create(validated_data)
@@ -31,7 +31,25 @@ class StockSerializer(serializers.ModelSerializer):
             objects.append(stock_item)
             StockProduct.objects.bulk_create(objects)
         return stock_product
-     
+    
+    
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions', [])
+        stock_product = super().update(instance, validated_data)
+        for position in positions:
+            objects = []
+            stock_item = StockProduct(stock_product, **position)
+            objects.append(stock_item)
+            StockProduct.objects.update_or_create(stock=objects[0], product=objects[1], defaults={'price': 1, 'quantity': 1})
+        return stock_product    
+
+
+
+
+
+
+
+
     
 
  
